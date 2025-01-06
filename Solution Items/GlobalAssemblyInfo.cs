535864b272cb6cc75c03cbf9119055c08cea9a76
ZhangShanwei <zhangshanwei@PC075>
2025-01-06 18:21:16 +0800

    1.6.6.2

--- Solution Items/GlobalAssemblyInfo.cs
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.6.1")]
-[assembly: AssemblyFileVersion("1.6.6.1")]
+[assembly: AssemblyVersion("1.6.6.2")]
+[assembly: AssemblyFileVersion("1.6.6.2")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket")]
 [assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2010-2015")]
